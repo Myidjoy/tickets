1c88b31e4bccc29fa9deaa915fcfd8083e73492d
Evgenii Deviatov <evgenii_deviatov@epam.com>
2022-05-31 13:31:16 +0300

    fix: fix reducer

--- src/app/reducer.jsx
+++ src/app/reducer.jsx
@@ -1,12 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 import ticketsObject from './flights.json';
-// const {log} = console
+
 const tickets = ticketsObject.result.flights;
 tickets.forEach((ticket, i) => {ticket.id = i + 1;});
-// console.log(tickets[0].flight.legs[0])
+
 const initialState = {
-  tickets,
-  filtredTickets: [],
+  tickets,  
   oneChange: false,
   noChange: false,
   price: false,
@@ -18,30 +17,15 @@ const ticketsSlice = createSlice({
   name: 'tickets/slice',
   initialState,
   reducers: {
-    upAction(state){ 
-      if(state.filtredTickets.length){
-        state.filtredTickets =  state.filtredTickets.slice().sort((a, b) => a.flight.price.total.amount - b.flight.price.total.amount);
-      } else {
-        state.tickets = state.tickets.slice().sort((a, b) => a.flight.price.total.amount - b.flight.price.total.amount);
-      }      
+    upAction(state){       
+      state.tickets = state.tickets.slice().sort((a, b) => a.flight.price.total.amount - b.flight.price.total.amount);            
     },
-    downAction(state){  
-      if(state.filtredTickets.length){
-        state.filtredTickets = state.filtredTickets.slice().sort((a, b) => b.flight.price.total.amount - a.flight.price.total.amount);
-      } else {
-        state.tickets = state.tickets.slice().sort((a, b) => b.flight.price.total.amount - a.flight.price.total.amount);
-      }        
+    downAction(state){      
+      state.tickets = state.tickets.slice().sort((a, b) => b.flight.price.total.amount - a.flight.price.total.amount);              
     },
-    timeAction(state){
-      if(state.filtredTickets.length){
-        state.filtredTickets = state.filtredTickets.slice().sort((a, b) => a.flight.legs[0].duration - b.flight.legs[0].duration);
-      } else {
-        state.tickets = state.tickets.slice().sort((a, b) => a.flight.legs[0].duration - b.flight.legs[0].duration);
-      }       
-    },
-    changeFiltred(state, action){            
-      state.filtredTickets = action.payload;
-    },
+    timeAction(state){     
+      state.tickets = state.tickets.slice().sort((a, b) => a.flight.legs[0].duration - b.flight.legs[0].duration);          
+    },    
     changeStatus(state, action) {
       const [name, value] = action.payload;
       state[name] = value;
